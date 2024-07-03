6ffe0063021c48f10a19e07efd42198786908c47
Amrit Krishn <amritkrishn06072003@gmail.com>
2024-07-04 01:04:38 +0530

    done project

---
+++ flight.py
@@ -0,0 +1,334 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set()
+
+
+
+train_data = pd.read_excel(r"E:\Price pred\FlightData_Train.xlsx")
+
+pd.set_option('display.max_columns', None)
+
+train_data.head()
+
+train_data.info()
+
+train_data["Duration"].value_counts()
+
+train_data.dropna(inplace = True)
+
+train_data.isnull().sum()
+
+
+
+train_data["Journey_day"] = pd.to_datetime(train_data.Date_of_Journey, format="%d/%m/%Y").dt.day
+
+train_data["Journey_month"] = pd.to_datetime(train_data["Date_of_Journey"], format = "%d/%m/%Y").dt.month
+
+train_data.head()
+
+# Since we have converted Date_of_Journey column into integers, Now we can drop as it is of no use.
+
+train_data.drop(["Date_of_Journey"], axis = 1, inplace = True)
+
+# Departure time is when a plane leaves the gate. 
+# Similar to Date_of_Journey we can extract values from Dep_Time
+
+# Extracting Hours
+train_data["Dep_hour"] = pd.to_datetime(train_data["Dep_Time"]).dt.hour
+
+# Extracting Minutes
+train_data["Dep_min"] = pd.to_datetime(train_data["Dep_Time"]).dt.minute
+
+# Now we can drop Dep_Time as it is of no use
+train_data.drop(["Dep_Time"], axis = 1, inplace = True)
+
+train_data.head()
+
+# Arrival time is when the plane pulls up to the gate.
+# Similar to Date_of_Journey we can extract values from Arrival_Time
+
+# Extracting Hours
+train_data["Arrival_hour"] = pd.to_datetime(train_data.Arrival_Time).dt.hour
+
+# Extracting Minutes
+train_data["Arrival_min"] = pd.to_datetime(train_data.Arrival_Time).dt.minute
+
+# Now we can drop Arrival_Time as it is of no use
+train_data.drop(["Arrival_Time"], axis = 1, inplace = True)
+
+train_data.head()
+
+# Time taken by plane to reach destination is called Duration
+# It is the differnce betwwen Departure Time and Arrival time
+
+
+# Assigning and converting Duration column into list
+duration = list(train_data["Duration"])
+
+for i in range(len(duration)):
+    if len(duration[i].split()) != 2:    # Check if duration contains only hour or mins
+        if "h" in duration[i]:
+            duration[i] = duration[i].strip() + " 0m"   # Adds 0 minute
+        else:
+            duration[i] = "0h " + duration[i]           # Adds 0 hour
+
+duration_hours = []
+duration_mins = []
+for i in range(len(duration)):
+    duration_hours.append(int(duration[i].split(sep = "h")[0]))    # Extract hours from duration
+    duration_mins.append(int(duration[i].split(sep = "m")[0].split()[-1]))   # Extracts only minutes from duration
+
+# Adding duration_hours and duration_mins list to train_data dataframe
+
+train_data["Duration_hours"] = duration_hours
+train_data["Duration_mins"] = duration_mins
+
+train_data.drop(["Duration"], axis = 1, inplace = True)
+
+train_data.head()
+
+
+
+train_data["Airline"].value_counts()
+
+# From graph we can see that Jet Airways Business have the highest Price.
+# Apart from the first Airline almost all are having similar median
+
+# Airline vs Price
+sns.catplot(y = "Price", x = "Airline", data = train_data.sort_values("Price", ascending = False), kind="boxen", height = 6, aspect = 3)
+plt.show()
+
+# As Airline is Nominal Categorical data we will perform OneHotEncoding
+
+Airline = train_data[["Airline"]]
+
+Airline = pd.get_dummies(Airline, drop_first= True)
+
+Airline.head()
+
+train_data["Source"].value_counts()
+
+# Source vs Price
+
+sns.catplot(y = "Price", x = "Source", data = train_data.sort_values("Price", ascending = False), kind="boxen", height = 4, aspect = 3)
+plt.show()
+
+# As Source is Nominal Categorical data we will perform OneHotEncoding
+
+Source = train_data[["Source"]]
+
+Source = pd.get_dummies(Source, drop_first= True)
+
+Source.head()
+
+train_data["Destination"].value_counts()
+
+# As Destination is Nominal Categorical data we will perform OneHotEncoding
+
+Destination = train_data[["Destination"]]
+
+Destination = pd.get_dummies(Destination, drop_first = True)
+
+Destination.head()
+
+train_data["Route"]
+
+# Additional_Info contains almost 80% no_info
+# Route and Total_Stops are related to each other
+
+train_data.drop(["Route", "Additional_Info"], axis = 1, inplace = True)
+
+train_data["Total_Stops"].value_counts()
+
+# As this is case of Ordinal Categorical type we perform LabelEncoder
+# Here Values are assigned with corresponding keys
+
+train_data.replace({"non-stop": 0, "1 stop": 1, "2 stops": 2, "3 stops": 3, "4 stops": 4}, inplace = True)
+
+train_data.head()
+
+# Concatenate dataframe --> train_data + Airline + Source + Destination
+
+data_train = pd.concat([train_data, Airline, Source, Destination], axis = 1)
+
+data_train.head()
+
+data_train.drop(["Airline", "Source", "Destination"], axis = 1, inplace = True)
+
+data_train.head()
+
+data_train.shape
+
+test_data = pd.read_excel(r"E:\Price pred\FlightTest_set.xlsx")
+
+test_data.head()
+print(test_data['Airline'].unique())
+# Preprocessing
+
+print("Test data Info")
+print("-"*75)
+print(test_data.info())
+
+print()
+print()
+
+print("Null values :")
+print("-"*75)
+test_data.dropna(inplace = True)
+print(test_data.isnull().sum())
+
+# EDA
+
+# Date_of_Journey
+test_data["Journey_day"] = pd.to_datetime(test_data.Date_of_Journey, format="%d/%m/%Y").dt.day
+test_data["Journey_month"] = pd.to_datetime(test_data["Date_of_Journey"], format = "%d/%m/%Y").dt.month
+test_data.drop(["Date_of_Journey"], axis = 1, inplace = True)
+
+# Dep_Time
+test_data["Dep_hour"] = pd.to_datetime(test_data["Dep_Time"]).dt.hour
+test_data["Dep_min"] = pd.to_datetime(test_data["Dep_Time"]).dt.minute
+test_data.drop(["Dep_Time"], axis = 1, inplace = True)
+
+# Arrival_Time
+test_data["Arrival_hour"] = pd.to_datetime(test_data.Arrival_Time).dt.hour
+test_data["Arrival_min"] = pd.to_datetime(test_data.Arrival_Time).dt.minute
+test_data.drop(["Arrival_Time"], axis = 1, inplace = True)
+
+# Duration
+duration = list(test_data["Duration"])
+
+for i in range(len(duration)):
+    if len(duration[i].split()) != 2:    # Check if duration contains only hour or mins
+        if "h" in duration[i]:
+            duration[i] = duration[i].strip() + " 0m"   # Adds 0 minute
+        else:
+            duration[i] = "0h " + duration[i]           # Adds 0 hour
+
+duration_hours = []
+duration_mins = []
+for i in range(len(duration)):
+    duration_hours.append(int(duration[i].split(sep = "h")[0]))    # Extract hours from duration
+    duration_mins.append(int(duration[i].split(sep = "m")[0].split()[-1]))   # Extracts only minutes from duration
+
+# Adding Duration column to test set
+test_data["Duration_hours"] = duration_hours
+test_data["Duration_mins"] = duration_mins
+test_data.drop(["Duration"], axis = 1, inplace = True)
+
+
+# Categorical data
+
+print("Airline")
+print("-"*75)
+print(test_data["Airline"].value_counts())
+Airline = pd.get_dummies(test_data["Airline"], drop_first= True)
+
+print()
+
+print("Source")
+print("-"*75)
+print(test_data["Source"].value_counts())
+Source = pd.get_dummies(test_data["Source"], drop_first= True)
+
+print()
+
+print("Destination")
+print("-"*75)
+print(test_data["Destination"].value_counts())
+Destination = pd.get_dummies(test_data["Destination"], drop_first = True)
+
+# Additional_Info contains almost 80% no_info
+# Route and Total_Stops are related to each other
+test_data.drop(["Route", "Additional_Info"], axis = 1, inplace = True)
+
+# Replacing Total_Stops
+test_data.replace({"non-stop": 0, "1 stop": 1, "2 stops": 2, "3 stops": 3, "4 stops": 4}, inplace = True)
+
+# Concatenate dataframe --> test_data + Airline + Source + Destination
+data_test = pd.concat([test_data, Airline, Source, Destination], axis = 1)
+
+data_test.drop(["Airline", "Source", "Destination"], axis = 1, inplace = True)
+data_test['Truejet']=0
+
+
+print("Shape of test data : ", data_test.shape)
+
+
+
+data_test.head()
+
+data_train.shape
+
+data_train.columns
+
+X = data_train.loc[:, ['Total_Stops', 'Journey_day', 'Journey_month', 'Dep_hour',
+       'Dep_min', 'Arrival_hour', 'Arrival_min', 'Duration_hours',
+       'Duration_mins', 'Airline_Air India', 'Airline_GoAir', 'Airline_IndiGo',
+       'Airline_Jet Airways', 'Airline_Jet Airways Business',
+       'Airline_Multiple carriers',
+       'Airline_Multiple carriers Premium economy', 'Airline_SpiceJet',
+       'Airline_Trujet', 'Airline_Vistara', 'Airline_Vistara Premium economy',
+       'Source_Chennai', 'Source_Delhi', 'Source_Kolkata', 'Source_Mumbai',
+       'Destination_Cochin', 'Destination_Delhi', 'Destination_Hyderabad',
+       'Destination_Kolkata', 'Destination_New Delhi']]
+X.head()
+
+y = data_train.iloc[:, 1]
+y.head()
+
+# Important feature using ExtraTreesRegressor
+
+# from sklearn.ensemble import ExtraTreesRegressor
+# selection = ExtraTreesRegressor()
+# selection.fit(X, y)
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+print("1111111111")
+print(X_train.shape)
+print(X_train.columns)
+print(data_test.columns)
+from sklearn.ensemble import RandomForestRegressor
+reg_rf = RandomForestRegressor()
+reg_rf.fit(X_train, y_train)
+
+y_pred = reg_rf.predict(X_test)
+
+reg_rf.score(X_train, y_train)
+
+reg_rf.score(X_test, y_test)
+
+sns.distplot(y_test-y_pred)
+plt.show()
+
+
+plt.scatter(y_test, y_pred, alpha = 0.5)
+plt.xlabel("y_test")
+plt.ylabel("y_pred")
+plt.show()
+
+from sklearn import metrics
+
+print('MAE:', metrics.mean_absolute_error(y_test, y_pred))
+print('MSE:', metrics.mean_squared_error(y_test, y_pred))
+print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+# RMSE/(max(DV)-min(DV))
+
+2090.5509/(max(y)-min(y))
+
+metrics.r2_score(y_test, y_pred)
+
+import pickle
+# open a file, where you ant to store the data
+file = open('flight_rf.pkl', 'wb')
+
+# dump information to that file
+pickle.dump(reg_rf, file)
+
+model = open('flight_rf.pkl','rb')
+forest = pickle.load(model)
